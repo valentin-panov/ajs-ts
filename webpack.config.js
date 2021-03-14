c050f37b203da747fd0c27d56f706b9ea42a37c0
Valentin Panov <valentin.panov@me.com>
2021-03-14 15:20:35 +0300

    eslint fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
